eab80888f2c3d739b2f849484d88bc15031e148d
mdan22 <mdan22@proton.me>
2024-09-30 13:22:54 +0200

    initial commit

---
+++ backend/utils/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
+
+const config = {
+  MONGODB_URI,
+  PORT
+}
+
+module.exports = config
