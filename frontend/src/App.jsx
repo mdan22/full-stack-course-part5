eba7f9e51dc83061cfe1519047b4c03f5311b140
mdan22 <mdan22@proton.me>
2024-09-30 20:52:13 +0200

    add solution for 5.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,24 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  // effect hooks should be put at beginning lol
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  // add effect hook for checking local storage of browser for user data
+  // and set user to logged in state if data was found 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   // outsource login form
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -36,7 +54,7 @@ const App = () => {
     </form>      
   )
 
-  // outsource blog form (comes later)
+  // outsource blog form (comes later maybe)
   // const blogForm = () => (
   //   <form onSubmit={addBlog}>
   //     <input
@@ -46,6 +64,12 @@ const App = () => {
   //     <button type="submit">save</button>
   //   </form>  
   // )
+
+  // outsource logout form
+  // simple - it's just a logout button
+  const logoutForm = () => (
+    <button onClick={handleLogout}>logout</button>
+  )
 
   // outsource blog list
   const blogList = () => {
@@ -66,33 +90,54 @@ const App = () => {
         username, password,
       })
 
+      // add user data to local storage of browser
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
+      // we could put an error message like this:
+      // setErrorMessage('Wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+      console.log(exception)
     }
   }
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  // handler sets user and token to null and
+  // removes local storage data of user in browser
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    
+    try {
+      window.localStorage.removeItem('loggedBlogAppUser')
+
+      blogService.setToken(null)
+      setUser(null)
+    } catch (exception) {
+      // we could put an error message like this:
+      // setErrorMessage('Wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+      console.log(exception)
+    }
+  }
 
   return (
     <div>
       <h2>blogs</h2>
 
-      {/* render login or blog form conditionally */}
+      {/* render login or (logout form and blog form) conditionally */}
       {user === null ?
         loginForm() :
         <div>
-          <p>{user.name} logged-in</p>
+          <p>{user.name} logged-in{logoutForm()}</p>
           {blogList()}
         </div>
       }
