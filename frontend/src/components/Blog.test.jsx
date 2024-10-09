dee84d411fac76a80d9d2687c03e8aaf02ab672d
mdan22 <mdan22@proton.me>
2024-10-09 13:14:27 +0200

    add solution for 5.15

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -60,10 +60,12 @@ describe('<Blog />', () => {
   })
 
   // 5.14
-  // test if children title and author are being
-  // rendered at start whilst URL and likes are not
+  // test if URL and likes are being rendered
+  // correctly after the view button has been clicked
   test('renders URL and likes after the view button has been clicked', async () => {
 
+    // click view button to reveal URL and likes attributes
+    // use getBy* queries if elements are expected to be in the DOM
     const button = screen.getByText('view')
     await user.click(button)
 
@@ -71,5 +73,25 @@ describe('<Blog />', () => {
     // use queryBy* queries if elements are expected to not be present or visible.
     expect(screen.getByText(testBlog.url)).toBeVisible()
     expect(screen.getByText(`likes ${testBlog.likes}`)).toBeVisible()
+  })
+
+  // 5.15
+  // test if event handler is called twice after like button is clicked twice
+  test('calls event handler twice after like button is clicked twice', async () => {
+
+    // click view button first to reveal like button
+    // use getBy* queries if elements are expected to be in the DOM
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    // click like button twice
+    // use getBy* queries if elements are expected to be in the DOM
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    // check that URL and likes are visible
+    // by accessing mock.calls of the mock handler onLike
+    expect(onLike.mock.calls).toHaveLength(2)
   })
 })
